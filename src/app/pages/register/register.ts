8126713cedd24006eea4e7a1d21f3cf523083791
Naf9999 <mnafta333@gmail.com>
2026-07-03 23:57:47 +0300

    form validation

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,35 +1,57 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
-  user = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    phone: '',
-    password: '',
-    confirmPassword: '',
-  };
+  registerForm!: FormGroup;
 
-  users: any[] = [];
 
-  onSubmit() {
-    this.users.push({ ...this.user });
-    console.log('Registered Users:', this.users);
-    // Reset the form after submission
-    this.user = {
-      firstName: '',
-      lastName: '',
-      email: '',
-      phone: '',
-      password: '',
-      confirmPassword: '',
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]
+    }, {
+      validator: this.passwordMatchValidator('password', 'confirmPassword')
+    }
+    );
+  }
+  
+  passwordMatchValidator(password: string, confirmPassword: string) {
+    return (formGroup: FormGroup) => {
+      const passwordControl = formGroup.controls[password];
+      const confirmPasswordControl = formGroup.controls[confirmPassword];
+
+      if (confirmPasswordControl.errors && !confirmPasswordControl.errors['passwordMismatch']) {
+        return;
+      }
+      
+      if (passwordControl.value !== confirmPasswordControl.value) {
+        confirmPasswordControl.setErrors({ passwordMismatch: true });
+      } else {
+        confirmPasswordControl.setErrors(null);
+      }
     };
   }
- }
+
+  onSubmit(): void {
+    if (this.registerForm.valid) {
+      console.log('Form submitted Successfully!', this.registerForm.value);
+    
+    } else {
+      this.registerForm.markAllAsTouched();
+      console.log('Form is invalid. Please fill in all required fields correctly.');
+    }
+  }
+
+}
